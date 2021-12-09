e5f4439d90d7c163b9aa49d6a2d0b83d00807a6f
rin <75649277+officialrin@users.noreply.github.com>
2021-12-09 01:30:24 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -19,7 +19,7 @@ def webServer(port=13331):
                 f = open(filename[1:])
                 outputdata = f.read()
                 # Send one HTTP header line into socket.
-                connectionSocket.send("HTTP/ 200 OK")
+                connectionSocket.send("HTTP/1.1 200 OK")
                 # Send the content of the requested file to the client
                 for i in range(0, len(outputdata)):
                     connectionSocket.send(outputdata[i].encode())
@@ -27,7 +27,7 @@ def webServer(port=13331):
                 connectionSocket.close()
             except IOError:
         # Send response message for file not found (404)
-                connectionSocket.send("HTTP/ 404 Not Found")
+                connectionSocket.send("HTTP/1.1 404 Not Found")
         # Close client socket
                 connectionSocket.close()
         except (ConnectionResetError, BrokenPipeError):
